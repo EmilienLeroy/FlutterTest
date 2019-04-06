8823b223652877303ddf4436883d7a3b916d516d
Emilien <admin@emilienleroy.fr>
2019-04-06 11:56:10 +0200

    add: style grid item

---
+++ lib/data/pokemons.dart
@@ -0,0 +1,25 @@
+import 'package:test_flutter/models/Pokemon.dart';
+import 'package:flutter/material.dart';
+
+final List<Pokemon> pokemons = [
+  new Pokemon("Pikachu", "Electrique", new AssetImage('assets/pikachu.png'),'pikachu.wav'),
+  new Pokemon("Salameche", "Feu", new AssetImage('assets/salameche.png'),'salameche.mp3'),
+  new Pokemon("Bulbizarre", "Plante", new AssetImage('assets/bulbizarre.png'),'bulbizarre.mp3'),
+  new Pokemon("Carapuce", "Eau", new AssetImage('assets/carapuce.png'),'carapuce.mp3'),
+  new Pokemon("Pikachu", "Electrique", new AssetImage('assets/pikachu.png'),'pikachu.wav'),
+  new Pokemon("Salameche", "Feu", new AssetImage('assets/salameche.png'),'salameche.mp3'),
+  new Pokemon("Bulbizarre", "Plante", new AssetImage('assets/bulbizarre.png'),'bulbizarre.mp3'),
+  new Pokemon("Carapuce", "Eau", new AssetImage('assets/carapuce.png'),'carapuce.mp3'),
+  new Pokemon("Pikachu", "Electrique", new AssetImage('assets/pikachu.png'),'pikachu.wav'),
+  new Pokemon("Salameche", "Feu", new AssetImage('assets/salameche.png'),'salameche.mp3'),
+  new Pokemon("Bulbizarre", "Plante", new AssetImage('assets/bulbizarre.png'),'bulbizarre.mp3'),
+  new Pokemon("Carapuce", "Eau", new AssetImage('assets/carapuce.png'),'carapuce.mp3'),
+  new Pokemon("Pikachu", "Electrique", new AssetImage('assets/pikachu.png'),'pikachu.wav'),
+  new Pokemon("Salameche", "Feu", new AssetImage('assets/salameche.png'),'salameche.mp3'),
+  new Pokemon("Bulbizarre", "Plante", new AssetImage('assets/bulbizarre.png'),'bulbizarre.mp3'),
+  new Pokemon("Carapuce", "Eau", new AssetImage('assets/carapuce.png'),'carapuce.mp3'),
+  new Pokemon("Pikachu", "Electrique", new AssetImage('assets/pikachu.png'),'pikachu.wav'),
+  new Pokemon("Salameche", "Feu", new AssetImage('assets/salameche.png'),'salameche.mp3'),
+  new Pokemon("Bulbizarre", "Plante", new AssetImage('assets/bulbizarre.png'),'bulbizarre.mp3'),
+  new Pokemon("Carapuce", "Eau", new AssetImage('assets/carapuce.png'),'carapuce.mp3')
+];
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:test_flutter/screens/about.dart';
 import 'package:test_flutter/screens/random.dart';
 import 'package:test_flutter/screens/chat.dart';
 import 'package:test_flutter/models/Pokemon.dart';
+import 'package:test_flutter/data/pokemons.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,12 +11,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
-  final List<Pokemon> pokemons = [
-    new Pokemon("Pikachu", "Electrique", new AssetImage('assets/pikachu.png'),'pikachu.wav'),
-    new Pokemon("Salameche", "Feu", new AssetImage('assets/salameche.png'),'salameche.mp3'),
-    new Pokemon("Bulbizarre", "Plante", new AssetImage('assets/bulbizarre.png'),'bulbizarre.mp3'),
-    new Pokemon("Carapuce", "Eau", new AssetImage('assets/carapuce.png'),'carapuce.mp3')
-  ];
+
 
   Drawer getNavDrawer(BuildContext context) {
     var headerChild = new DrawerHeader(child: new Text("Header"));
@@ -66,28 +62,60 @@ class HomeScreenState extends State<HomeScreen> {
                 itemBuilder:  (BuildContext context, int index){
                   final pokemon = pokemons[index];
 
-                  return new GestureDetector(
+                  return GridTile(
+                    child: new GestureDetector(
                       child: new Card(
-                      elevation: 5.0,
-                      semanticContainer: true,
-                      child: new Container(
-                        decoration: new BoxDecoration(
-                          image: new DecorationImage(image: pokemon.img,fit: BoxFit.cover),
-                          borderRadius: new BorderRadius.all(new Radius.circular(5)),
+                        elevation: 5.0,
+                        semanticContainer: true,
+                        margin: const EdgeInsets.all(10.0),
+                        shape:  RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
                         ),
-                        alignment: Alignment.center,
-                        child: new Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(pokemon.name),
-                            Text(pokemon.type)
-                          ]
+                        child: new Container(
+                            decoration: new BoxDecoration(
+                              image: new DecorationImage(image: pokemon.img,fit: BoxFit.cover),
+                              borderRadius: new BorderRadius.all(new Radius.circular(5)),
+                              gradient: LinearGradient(
+                                // Where the linear gradient begins and ends
+                                begin: Alignment.bottomLeft,
+                                end: Alignment.topRight,
+                                // Add one stop for each color. Stops should increase from 0 to 1
+                                stops: [0.1, 0.5, 0.7, 0.9],
+                                colors: [
+                                  // Colors are easy thanks to Flutter's Colors class.
+                                  Color(0xff845ec2),
+                                  Color(0xffe65faa),
+                                  Color(0xffff8080),
+                                  Color(0xfff9f871),
+                                ],
+                              ),
+                            ),
+                            alignment: Alignment.center,
                         ),
                       ),
-                    ),
-                    onTap: (){
+                      onTap: (){
                         pokemon.sound();
-                    },
+                      },
+                    ),
+                    footer: new GestureDetector(
+                      child: new Container(
+                        margin: const EdgeInsets.all(10.0),
+                        padding: const EdgeInsets.all(10.0),
+                        width: double.infinity,
+                        decoration: new BoxDecoration(
+                          color: Colors.black45,
+                          borderRadius: new BorderRadius.all(new Radius.circular(5)),
+                        ),
+                        child: new Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('Nom: ${pokemon.name}',style: TextStyle(color: Colors.white)),
+                            Text('Type: ${pokemon.type}',style: TextStyle(color: Colors.white)),
+                          ],
+                        )
+                        ,
+                      ),
+                    ),
                   );
                 }
             )
